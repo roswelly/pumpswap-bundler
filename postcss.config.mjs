7965f3c2843792e4448f063ff5e8e4cadb26e963
0xzepdev <yabidev@gmail.com>
2025-07-07 04:52:13 -0700

    migration

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -4,5 +4,5 @@ const config = {
     tailwindcss: {},
   },
 };
-
+// 
 export default config;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,5 @@
 import type { Config } from "tailwindcss";
-
+// 
 export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
